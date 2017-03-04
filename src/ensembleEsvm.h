54b3de13dff239a046ff3d86824a3bd02bfbf683
livia <sam.n.johnston@gmail.com>
2017-03-04 13:17:22 -0500

    Most basic (not working)

--- src/ensembleEsvm.h
+++ src/ensembleEsvm.h
@@ -16,43 +16,30 @@ class EnsembleESVM
 {
 public:
     EnsembleESVM();
-    // EnsembleESVM(std::vector<FeatureVector> positives, std::vector<FeatureVector> negatives, std::string id = "");
-    // EnsembleESVM(std::string trainingSamplesFilePath, std::string id = "");
-    // EnsembleESVM(svm_model* trainedModel, std::string id = "");   
-    // void readSampleDataFile(std::string filePath, std::vector<FeatureVector>& sampleFeatureVectors, 
-    //                         std::vector<int>& targetOutputs, FileFormat format = LIBSVM);
-    // void readSampleDataFile(std::string filePath, std::vector<FeatureVector>& sampleFeatureVectors, FileFormat format = LIBSVM);
-    // void writeSampleDataFile(std::string filePath, std::vector<FeatureVector>& sampleFeatureVectors, 
-    //                          std::vector<int>& targetOutputs, FileFormat format = LIBSVM);
-    // bool saveModelFile(std::string modelFilePath);
     std::vector<double> predict(const cv::Mat roi);
     inline std::string getTargetID() { return targetID; }
 
 private:
-    // void trainEnsembleModel(std::vector<FeatureVector> samples, std::vector<int> targetOutputs, std::vector<double> classWeights);
-    // static std::vector<double> calcClassWeightsFromMode(int positivesCount, int negativesCount);
-    // static void readSampleDataFile_binary(std::string filePath, std::vector<FeatureVector>& sampleFeatureVectors, std::vector<int>& targetOutputs);
-    // static void readSampleDataFile_libsvm(std::string filePath, std::vector<FeatureVector>& sampleFeatureVectors, std::vector<int>& targetOutputs);
-    // static void writeSampleDataFile_binary(std::string filePath, std::vector<FeatureVector>& sampleFeatureVectors, std::vector<int>& targetOutputs);
-    // static void writeSampleDataFile_libsvm(std::string filePath, std::vector<FeatureVector>& sampleFeatureVectors, std::vector<int>& targetOutputs);
-    // static svm_node* getFeatureVector(FeatureVector features);
-    // static svm_node* getFeatureVector(double* features, int featureCount);
-    // svm_model* ensembleModel = nullptr;
+    void setContants();
+    // Constants
     std::string targetID;
-    std::vector<ESVM> ensembleEsvm;
-    std::vector<FeatureVector> probeSampleFeats;
+    xstd::mvector<1, FeatureVector> probeSampleFeats;
     size_t nPatches;
+    size_t nPositives;
     cv::Size imageSize;
     cv::Size patchCounts;
     cv::Size blockSize;
     cv::Size blockStride;
     cv::Size cellSize;
     int nBins;
+    std::string sampleFileExt;
+    FileFormat sampleFileFormat;
 
-     xstd::mvector<1, double> scores;
-     xstd::mvector<1, double> classificationScores;
+    xstd::mvector<2, double> scores;
+    xstd::mvector<1, double> classificationScores;
 
-     FeatureExtractorHOG hog;
+    FeatureExtractorHOG hog;
+    xstd::mvector<2, ESVM> ensembleEsvm; 
 
     double hogHardcodedFoundMin;            // Min found using 'FullChokePoint' test with SAMAN pre-generated files
     double hogHardcodedFoundMax;     // Max found using 'FullChokePoint' test with SAMAN pre-generated files
