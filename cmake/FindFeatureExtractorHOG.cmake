941945320456c2cd0aa4c9c852b89a6c2b1118b0
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2017-05-04 16:30:14 -0400

    separate cpp/h files + add basic cmake scripts

--- cmake/FindFeatureExtractorHOG.cmake
+++ cmake/FindFeatureExtractorHOG.cmake
@@ -1,14 +1,15 @@
-
-# - Find FeatureExtractorHOG (feHOG)
-# HOG feature extraction from image
+# - Find FeatureExtractorHOG
+# HOG feature extraction from image (MATLAB/C++)
 # available at https://bitbucket.org/KenjiKyoTeam//FeatureExtractorHOG/
 #
 # The module defines the following variables:
 #  feHOG_FOUND
 #  feHOG_ROOT_DIR
+
 #=============================================================================
 
-set( feHOG_ROOT_DIR "feHOG_ROOT_DIR-NOTFOUND" CACHE PATH "FeatureExtractorHOG root directory")
+# set paths
+set(feHOG_ROOT_DIR "feHOG_ROOT_DIR-NOTFOUND" CACHE PATH "FeatureExtractorHOG root directory")
 
 # handle REQUIRED and QUIET options
 include ( FindPackageHandleStandardArgs )
