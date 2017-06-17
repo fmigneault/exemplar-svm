33c42403b54e61995d66c5395216513beb5fa0a0
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2017-06-17 10:17:30 -0400

    backup cmake files

--- cmake/FindFeatureExtractorHOG.cmake
+++ cmake/FindFeatureExtractorHOG.cmake
@@ -4,14 +4,14 @@
 # available at https://bitbucket.org/KenjiKyoTeam//FeatureExtractorHOG/
 #
 # The module defines the following variables:
-#  FEHOG_FOUND
-#  FEHOG_ROOT_DIR
+#  feHOG_FOUND
+#  feHOG_ROOT_DIR
 #=============================================================================
 
-set( FEHOG_ROOT_DIR "FEHOG_ROOT_DIR-NOTFOUND" CACHE PATH "FeatureExtractorHOG root directory")
+set( feHOG_ROOT_DIR "feHOG_ROOT_DIR-NOTFOUND" CACHE PATH "FeatureExtractorHOG root directory")
 
 # handle REQUIRED and QUIET options
 include ( FindPackageHandleStandardArgs )
 find_package_handle_standard_args ( feHOG DEFAULT_MSG
-	FEHOG_ROOT_DIR	
+	feHOG_ROOT_DIR	
 )
